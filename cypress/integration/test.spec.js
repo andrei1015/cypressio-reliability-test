75dc87622fd8cfd98e4283dd4c822ae437fa2f96
Andrei <andrei1015@gmail.com>
2022-01-09 04:38:24 +0100

    have like an actual test that makes sense and has a bit of logic

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,9 +1,8 @@
 describe('example test', () => {
-  beforeEach(() => {
-    cy.visit('http://www.randomnumberapi.com/api/v1.0/random?min=1&max=2&count=1')
-  })
 
   it('step', () => {
-    
+    cy.request('http://www.randomnumberapi.com/api/v1.0/random?min=1&max=2&count=1').then((response) => {
+      console.log(response.body)
+    })
   })
 })
