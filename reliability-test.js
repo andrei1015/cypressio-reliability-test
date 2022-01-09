6616caf8bd1d686e29359724dbf75c0e91a60f77
Andrei <andrei1015@gmail.com>
2022-01-09 14:26:00 +0100

    javascript sucks

--- reliability-test.js
+++ reliability-test.js
@@ -2,10 +2,16 @@ const { exec } = require("child_process");
 process.env['BROWSERSLIST_IGNORE_OLD_DATA'] = true;
 
 let runNumber = 0
-var passes = 0
-var fails = 0
+// let passes
+// let fails
+// let passes = 0
+// let fails = 0
 
 function testRuns() {
+// let passes
+// let fails
+var passes = 0
+var fails = 0
     for (let i = 0; i < 2; i++) {
         exec("npx cypress run", (error, stdout, stderr) => {
             let splitLines = stdout.split(/\r?\n/);
@@ -15,27 +21,46 @@ function testRuns() {
 
             if ( lastLine.includes('failed') ) {
                 fails = fails + 1
+                // console.log("fails: " + fails)
+                // console.log("passes: " + passes) 
             }
 
-            else if ( lastLine.includes('passed') ) {
-                passes = passes + 1
-            }
+            // else if ( lastLine.includes('passed') ) {
+            //     passes = passes + 1
+            //     // console.log("fails: " + fails)
+            //     // console.log("passes: " + passes) 
+            // }
+            //  console.log("fails: " + fails)
+            //  console.log("passes: " + passes)  
         })
     }
-    return fails, passes
+    // console.log( fails, passes )
+    //totals()
+    return fails
+    // callback()
+
 }
 
-testRuns()
+testRuns(() => {
+    console.log(fails)
+    //console.log(passes)
+})
 
-function totals(fails, passes) {
-    console.log("fails: " + fails)
-    console.log("passes: " + passes)  
-}
+console.log(testRuns())
 
-totals()
+// testRuns(fails, passes, function() {
+//     // await testRuns(() => {
+//         console.log('cacat' + passes)
+//         console.log(fails)
+//     // }) 
+//     // const cacat = await testRuns()
+//     // console.log(cacat)
+// })
 
+// const pipi =  testRuns()
+// console.log(pipi)
 
-
+// totals()
 
 
 // video mutat
