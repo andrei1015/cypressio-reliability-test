862a7c61505ed5382f47c44ccbbeb0aad68e4796
Andrei <andrei1015@gmail.com>
2022-01-09 04:33:06 +0100

    cypressio installed and a sample file

---
+++ cypress/integration/1-getting-started/test.spec.js
@@ -0,0 +1,9 @@
+describe('example test', () => {
+  beforeEach(() => {
+    cy.visit('http://www.randomnumberapi.com/api/v1.0/random?min=1&max=2&count=1')
+  })
+
+  it('step', () => {
+    
+  })
+})
