69a8f0dcbe06355fb26e7fe9378f75760abb01ef
Andrei <andrei1015@gmail.com>
2022-01-09 04:44:33 +0100

    bring improvements toward thy code

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,8 +1,12 @@
 describe('example test', () => {
 
   it('step', () => {
-    cy.request('http://www.randomnumberapi.com/api/v1.0/random?min=1&max=2&count=1').then((response) => {
+
+    // for what it matters here we are using a random number generator to roughly simulate an inconsistent test, we choose 1 value out of 5 possible, so fail or pass
+
+    cy.request('http://www.randomnumberapi.com/api/v1.0/random?min=1&max=5&count=1').then((response) => {
       console.log(response.body)
+      expect(response.body[0]).to.not.equal(1)
     })
   })
 })
