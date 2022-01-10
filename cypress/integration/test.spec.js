fe949ba13c0dd2c1a7270bd3ec62fa0909c4608d
Andrei <andrei1015@gmail.com>
2022-01-10 22:38:34 +0100

    browserlist pls

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,7 +1,6 @@
-//Cypress.env('BROWSERSLIST_IGNORE_OLD_DATA')
+Cypress.env('BROWSERSLIST_IGNORE_OLD_DATA')
 describe('example test', () => {
   it('step', () => {
-    //console.log('var ' + Cypress.env('BROWSERSLIST_IGNORE_OLD_DATA'))
     // for what it matters here we are using a random number generator to roughly simulate an inconsistent test, we choose 1 value out of 5 possible, so fail or pass
 
     cy.request('http://www.randomnumberapi.com/api/v1.0/random?min=1&max=5&count=1').then((response) => {
