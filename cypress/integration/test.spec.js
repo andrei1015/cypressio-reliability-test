b78fe90b3bfd5db62ae49731a783eb862b96c4cc
Andrei <andrei1015@gmail.com>
2022-01-11 00:06:42 +0100

    bash working

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -3,7 +3,7 @@ describe('example test', () => {
   it('step', () => {
     // for what it matters here we are using a random number generator to roughly simulate an inconsistent test, we choose 1 value out of 5 possible, so fail or pass
 
-    cy.request('http://www.randomnumberapi.com/api/v1.0/random?min=1&max=5&count=1').then((response) => {
+    cy.request('http://www.randomnumberapi.com/api/v1.0/random?min=1&max=2&count=1').then((response) => {
       console.log(response.body)
       expect(response.body[0]).to.not.equal(1)
     })
--- reliability-test.sh
+++ reliability-test.sh
@@ -4,11 +4,26 @@ date=`date +%Y%m%d%H%M%S`
 echo $date
 report_file=reports/${date}.txt
 
+number_of_times=$2
+echo $number_of_times
+
 touch $report_file
 
-for (( i=1; i<=5; i++ ))
+for (( i=1; i<=$number_of_times; i++ ))
 do
-echo 'run: ' $i
-npx cypress run | tac | sed -n '2 p' >> $report_file
-echo "ran"
+    echo 'run: ' $i
+    npx cypress run $1 | tac | sed -n '2 p' >> $report_file
+    last_line=$(cat $report_file | sed -n '$p')
+    
+    echo $last_line
+    
+    if [[ "$last_line" == *"passed"* ]]
+    then
+        ((passes=passes+1))
+    else
+        ((fails=fails+1))
+    fi
 done
+
+echo "passes: $passes"
+echo "fails: $fails"
