b2346007949126fba05eed462caa757c5de36c78
Andrei <andrei1015@gmail.com>
2022-01-10 17:15:04 +0100

    sometimes works

--- reliability-test.js
+++ reliability-test.js
@@ -1,63 +1,44 @@
 process.env['BROWSERSLIST_IGNORE_OLD_DATA'] = true; // deal with spaghetti code and stuff
 
-const { exec } = require("child_process");
+const { execSync } = require("child_process");
 const fs = require('fs');
 var dayjs = require('dayjs');
-const { callbackify } = require("util");
 
-let passes
-let fails
+let passes = 0
+let fails = 0 
 let lastLine
-passes = 0
-fails = 0
+
 
 let date = dayjs().format('YYYYMMDDHHHmmss')
 const filePath = 'reports/' + date + '.txt'
 
-
-async function testRuns(callback) {
+async function testRuns() {
     for (let i = 0; i < 2; i++) {
-        exec("npx cypress run", (error, stdout, stderr) => {
+            let stdout = execSync('npx cypress run').toString()
             let splitLines = stdout.split(/\r?\n/);
             lastLine = splitLines[splitLines.length - 3] // it's not actually the last line but it's what we need
 
-            totalsSum()
-
-            fs.appendFile(
-                filePath, 
-                'run ' + i  + ' :' + lastLine + '\n', 
-                { flag: 'a' }, 
-                function (err) {
-                    if (err) throw err;
-                });
-
+            if ( lastLine.includes('failed') ) {
+                fails = fails + 1
+            }
+            else if ( lastLine.includes('passed') ) {
+                passes = passes + 1
+            }
+    
+            // fs.appendFile(
+            //     filePath, 
+            //     'run ' + i  + ' :' + lastLine + '\n', 
+            //     { flag: 'a' });
             console.log(lastLine);
-        })
     }
-    return [passes, fails]
-}
-
-function totalsSum() {
-    if ( lastLine.includes('failed') ) {
-        fails = fails + 1
-    }
-    else if ( lastLine.includes('passed') ) {
-        passes = passes + 1
-    }
-}
-
-function display(fails, passes) {
     console.log('1')
     console.log(passes)
     console.log(fails)
     console.log('2')
+    //display()
 }
 
-async function run() {
-    testRuns();
-    await display(fails, passes);
-}
-
-run()
+// run()
+testRuns()
 
 // video moving code goes here
