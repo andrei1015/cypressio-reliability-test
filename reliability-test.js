710065ecd17c3386e29ed82271404f22ebc5066c
Andrei <andrei1015@gmail.com>
2022-01-10 11:31:20 +0100

    cleanup + non-functional counters

--- reliability-test.js
+++ reliability-test.js
@@ -1,9 +1,14 @@
 const { exec } = require("child_process");
 const fs = require('fs');
+var dayjs = require('dayjs')
 process.env['BROWSERSLIST_IGNORE_OLD_DATA'] = true;
 
-let date = new Date();
-const filePath = '/reports/test.txt'
+let passes = 0
+let fails = 0
+let lastLine
+
+let date = dayjs().format('YYYYMMDDHHHmm')
+const filePath = 'reports/' + date + '.txt'
 
 const fd = fs.openSync(filePath, 'w')
 
@@ -12,16 +17,36 @@ function testRuns() {
     for (let i = 0; i < 2; i++) {
         exec("npx cypress run", (error, stdout, stderr) => {
             let splitLines = stdout.split(/\r?\n/);
-            let lastLine = splitLines[splitLines.length - 3] // it's not actually the last line but it's what we need
+            lastLine = splitLines[splitLines.length - 3] // it's not actually the last line but it's what we need
 
-            fs.appendFile(filePath, 'This is run ' + i  + ' :' + lastLine, { flag: 'w' }, function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-            });
+            totalsSum()
+            display()
+            fs.appendFile(
+                filePath, 
+                'run ' + i  + ' :' + lastLine + '\n', 
+                { flag: 'a' }, 
+                function (err) {
+                    if (err) throw err;
+                });
 
             console.log(lastLine);
         })
     }
+    display()
+}
+
+function totalsSum() {
+    if ( lastLine.includes('failed') ) {
+        fails = fails + 1
+    }
+    else if ( lastLine.includes('passed') ) {
+        passes = passes + 1
+    }
+}
+
+function display() {
+    console.log(passes)
+    console.log(fails)
 }
 
 testRuns()
