498ba33f41ddb8ccc19f7442f1cdbbaff6658bc2
Andrei <andrei1015@gmail.com>
2022-01-11 03:27:35 +0100

    add meme

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-Run in bash:
+**Run in bash**:
 
 `sh reliability-test.sh <test-file-inside-cypress> <tries>`
+
+
+### What's with the browserlist warning??
+🤷🤷🤷🤷🤷🤷🤷
+I have no idea. Ask cypress. Ask the browserlist people.
+I only care it remains in the terminal and doesn't reach the report file.
+
+### How does the browserlist warning bypass linux's sed??
+Hell if I know...
+
+### Why does the browserlist warning keep happening after 2 separate attempts at supressing it??
+Beats me...
+
+### What's with the similar looking javascript file??
+We talk to js once this meme is not relevant anymore
+![meme image](meme.png "please be normal js")
--- reliability-test.sh
+++ reliability-test.sh
@@ -4,7 +4,8 @@ date=`date +%Y%m%d%H%M%S`
 echo $date
 report_file=reports/${date}.txt
 videos_folder=reports/videos/${date}
-cypress_videos_folder=cypress/videos
+screenshots_folder=reports/screenshots/${date}
+cypress_folder=cypress
 
 number_of_times=$2
 echo $number_of_times
@@ -15,18 +16,23 @@ touch $report_file
 
 for (( i=1; i<=$number_of_times; i++ ))
 do
-    echo 'run: ' $i
+    echo -e '
+=================\n
+    run: ' $i '\n
+=================\n
+'
     npx cypress run $1 | tac | sed -n '2 p' | awk '{ printf "run '$i'"; print }'  >> $report_file
     last_line=$(cat $report_file | sed -n '$p')
     
-    echo $last_line
+    echo 'result' $last_line
     
     if [[ "$last_line" == *"passed"* ]]
     then
         ((passes+=1))
     else
         ((fails+=1))
-        mv ${cypress_videos_folder}/$1.mp4 ${videos_folder}/$i-$1.mp4
+        mv ${cypress_folder}/videos/$1.mp4 ${videos_folder}/$i-$1.mp4
+        mkdir -p ${screenshots_folder}/$i && mv -v ${cypress_folder}/screenshots/$1/* ${screenshots_folder}/$i/
     fi
 done
 
